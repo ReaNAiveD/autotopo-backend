9a2d5ee462af5af3d76ee3d77c95390d8307df03
NAiveD <nice-die@live.com>
2021-12-01 19:12:10 +0800

    init: Init AutoTopology Backend Project

---
+++ src/main/java/cn/svecri/autotopo/AutotopoApplication.java
@@ -0,0 +1,13 @@
+package cn.svecri.autotopo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutotopoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutotopoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/svecri/autotopo/AutotopoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.svecri.autotopo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutotopoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
