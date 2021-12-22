62be3338d9c5cdf0c1fa0f564af0632c1b44709b
lmt <171250019@smail.nju.edu.cn>
2021-12-22 16:00:11 +0800

    chore: simplify

--- src/main/java/cn/svecri/autotopo/controller/DeviceController.java
+++ src/main/java/cn/svecri/autotopo/controller/DeviceController.java
@@ -1,19 +1,26 @@
 package cn.svecri.autotopo.controller;
 
+import cn.svecri.autotopo.service.TopoConfigService;
+import cn.svecri.autotopo.service.TopoDeployService;
 import cn.svecri.autotopo.vo.Command;
 import cn.svecri.autotopo.vo.ResponseVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+
 @RestController
 @RequestMapping("/api/v1/topology/cmd")
 public class DeviceController {
+    @Autowired
+    TopoDeployService topoDeployService;
 
     @PostMapping("/exec")
     public ResponseVo<Object> execCmd(@RequestBody Command cmd) {
-        return ResponseVo.ok();
+        return ResponseVo.ok(topoDeployService.exec(Collections.singletonList(cmd)));
     }
 
 }
